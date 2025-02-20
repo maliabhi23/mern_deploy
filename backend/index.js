858457eddf27061cb8ccf267e6dc39178f2e9e85
Abhishek <154997025+maliabhi23@users.noreply.github.com>
2025-02-20 19:39:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const dbConnect = require("./config/database");
 const userRoutes = require("./routes/user");
 const app = express();
 var cors = require("cors");
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
